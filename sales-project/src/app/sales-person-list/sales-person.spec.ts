7f1eca0bbd11067b8f32c69aa2952c54360de982
fred <abnertorres58@gmail.com>
2022-05-06 02:16:38 -0400

    Angular Conditionals and Formatting

--- sales-project/src/app/sales-person-list/sales-person.spec.ts
+++ sales-project/src/app/sales-person-list/sales-person.spec.ts
@@ -2,6 +2,7 @@ import { SalesPerson } from './sales-person';
 
 describe('SalesPerson', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     expect(new SalesPerson()).toBeTruthy();
   });
 });
